6fc4c29be0fe56cc6b1bb89feb5a9276cb2f506c
Matthew Simpson <mcsimpson+gh@gmail.com>
2022-08-15 16:58:22 -0700

    remove more non-needed files. link scss files to page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bf__wrapper">
+      <header classname="header"></header>
+      <div className="hero"></div>
+      <div className="flexwrapper">
+        <section className="details"></section>
+        <div className="comments"></div>
+        <div className="nextvideos"></div>
+      </div>
     </div>
   );
 }
