6b991e67a477b01a14776be25ca8f3c353e7898f
Matthew Simpson <mcsimpson+gh@gmail.com>
2022-08-16 16:20:36 -0700

    header component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import bflogo from "./assets/images/BrainFlix-logo.svg";
+// TEMP
 import userimg from "./assets/images/Mohan-muruge.jpg";
+import bflogo from "./assets/images/BrainFlix-logo.svg";
+import upld from "./assets/images/Icons/upload.svg";
+
+// Not Temp
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="bf__wrapper">
-      <header classname="header">
-        <img src={bflogo} alt="BrainFlix" className="header__logo" />
-        <input
-          type="search"
-          placeholder="Search..."
-          className="header__searchbar"
-        ></input>
-        <div className="glbl__user-avatar header__avatar">
-          <img
-            className="glbl__user-avatar header__avatar"
-            alt="User Img"
-            src={userimg}
-          ></img>
-          <button className="glbl__button header__uploadbutton">Upload</button>
-        </div>
-      </header>
+      <Header userimg={userimg} bflogo={bflogo} upld={upld} />
+      {/* To Be Worked On */}
       <div className="hero"></div>
       <div className="flexwrapper">
         <section className="details"></section>
