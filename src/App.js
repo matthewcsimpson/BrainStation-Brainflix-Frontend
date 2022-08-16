96bd2ffa453fa3e370e1ed9663e780460b31a817
Matthew Simpson <mcsimpson+gh@gmail.com>
2022-08-15 17:25:46 -0700

    header elements in place, largely unstyled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import bflogo from "./assets/images/BrainFlix-logo.svg";
+import userimg from "./assets/images/Mohan-muruge.jpg";
+
 function App() {
   return (
     <div className="bf__wrapper">
-      <header classname="header"></header>
+      <header classname="header">
+        <img src={bflogo} alt="BrainFlix" className="header__logo" />
+        <input
+          type="search"
+          placeholder="Search..."
+          className="header__searchbar"
+        ></input>
+        <div className="glbl__user-avatar header__avatar">
+          <img
+            className="glbl__user-avatar header__avatar"
+            alt="User Img"
+            src={userimg}
+          ></img>
+          <button className="glbl__button header__uploadbutton">Upload</button>
+        </div>
+      </header>
       <div className="hero"></div>
       <div className="flexwrapper">
         <section className="details"></section>
